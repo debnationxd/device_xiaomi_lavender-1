9f89d9eb516e85c9be122deb61829059dbfb724e
Prashant-1695 <prashant33968@gmail.com>
2021-10-08 16:02:57 +0000

    dot-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/dot_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lavender-user \
-    aosp_lavender-userdebug \
-    aosp_lavender-eng
+    dot_lavender-user \
+    dot_lavender-userdebug \
+    dot_lavender-eng
--- dot_lavender.mk
+++ dot_lavender.mk
@@ -10,19 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common PixelExtended stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
+$(call inherit-product, vendor/dot/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-PEX_BUILD_TYPE := OFFICIAL
-PEX_MAINTAINER := Prashant
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := dot_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -35,7 +30,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="coral-user 11 RQ3A.210905.001 7511028 release-keys"
 
 BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.210905.001/7511028:user/release-keys"
-
-# Pixel Extended Device Maintainers
-PRODUCT_PROPERTY_OVERRIDES += \
-    org.pex.build_maintainer=Prashant
