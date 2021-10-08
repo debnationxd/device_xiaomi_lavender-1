d585b1818f1075441c303918afb03fc11364a065
Prashant <71384284+Prashant-1695@users.noreply.github.com>
2021-10-08 21:35:00 +0530

    Update dot_lavender.mk

--- dot_lavender.mk
+++ dot_lavender.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common PixelExtended stuff
-$(call inherit-product, vendor/dot/config/common_full_phone.mk) 
+$(call inherit-product, vendor/dot/config/common.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from lavender device
