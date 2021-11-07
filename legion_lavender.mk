1bc207f4b76790cd9c841fa1c5a190db30847ecd
debnationxd <71305844+debnationxd@users.noreply.github.com>
2021-11-07 10:49:14 +0530

    Update legion_lavender.mk

--- legion_lavender.mk
+++ legion_lavender.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common PixelExtended stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
+# Inherit some common Legion stuff
+$(call inherit-product, vendor/legion/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
