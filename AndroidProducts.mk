1c1e1058cae4be33a638f18a9201284fd5faecd7
debnationxd <71305844+debnationxd@users.noreply.github.com>
2021-11-07 10:40:16 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lavender.mk
+    $(LOCAL_DIR)/legion_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lavender-user \
-    aosp_lavender-userdebug \
-    aosp_lavender-eng
+    legion_lavender-user \
+    legion_lavender-userdebug \
+    legion_lavender-eng
