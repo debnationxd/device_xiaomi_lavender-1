a2490c3a898a6eaa97c08e16a2c00d4bf7f1ec63
debnationxd <71305844+debnationxd@users.noreply.github.com>
2021-11-10 14:13:50 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/legion_lavender.mk
+    $(LOCAL_DIR)/syberia_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    legion_lavender-user \
-    legion_lavender-userdebug \
-    legion_lavender-eng
+    syberia_lavender-user \
+    syberia_lavender-userdebug \
+    syberia_lavender-eng
