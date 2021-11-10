ea179a91152737309d3b76efd15a1cc733e9bd81
debnationxd <71305844+debnationxd@users.noreply.github.com>
2021-11-10 14:12:43 +0530

    Update and rename legion_lavender.mk to syberia_lavender.mk

--- syberia_lavender.mk
+++ syberia_lavender.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Legion stuff
-$(call inherit-product, vendor/legion/config/common_full_phone.mk) 
+$(call inherit-product, vendor/syberia/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
@@ -20,7 +20,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := legion_lavender
+PRODUCT_NAME := syberia_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
