e7506236ff302a68b16912439bfeaa2dcf102066
debnationxd <71305844+debnationxd@users.noreply.github.com>
2021-11-15 21:52:35 +0530

    posp bringup

--- syberia_lavender.mk
+++ syberia_lavender.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Legion stuff
-$(call inherit-product, vendor/syberia/config/common_full_phone.mk) 
+# Inherit some common POSP stuff
+$(call inherit-product, vendor/potato/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
@@ -20,7 +20,7 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := syberia_lavender
+PRODUCT_NAME := potato_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -33,3 +33,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="coral-user 11 RQ3A.211001.001 7641976 release-keys"
 
 BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys"
+# Posp stuff
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.potato.has_cutout=true 
